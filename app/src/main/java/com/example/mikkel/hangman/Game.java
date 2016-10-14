8ca17e480bd32d33b91a47ad3d193f6db5cdb946
Mikkel Hansen <mikkelhansen.92@jubii.dk>
2016-10-14 16:41:40 +0200

    Tilføjet onClickListener

--- app/src/main/java/com/example/mikkel/hangman/Game.java
+++ app/src/main/java/com/example/mikkel/hangman/Game.java
@@ -2,12 +2,40 @@ package com.example.mikkel.hangman;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class Game extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class Game extends AppCompatActivity implements View.OnClickListener{
+
+    Logic logic;
+    Button btn_guess;
+    TextView gameText;
+    EditText guessText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        logic = new Logic();
+        gameText = (TextView) findViewById(R.id.gameText);
+        guessText = (EditText) findViewById(R.id.guess);
+        btn_guess = (Button) findViewById(R.id.btn_guess);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == btn_guess) {
+            String guess = guessText.getText().toString();
+            if(guess.length() == 1) {
+                logic.gætBogstav(guess);
+            } else {
+
+            }
+        }
     }
 }
